1d2f2d327d96a10a5762213046c24bd763bc4e9f
mtorrisi <mario.torrisi@ingv.it>
2022-05-23 16:13:45 +0200

    Add Babel transpiling support for nullish-coaleshing operator and optional chaining in order to correctly use react-leaflet components.

--- invenio_assets/assets/build/webpack.config.js
+++ invenio_assets/assets/build/webpack.config.js
@@ -135,6 +135,8 @@ var webpackConfig = {
               plugins: [
                 "@babel/plugin-proposal-class-properties",
                 "@babel/plugin-transform-runtime",
+                "@babel/plugin-proposal-optional-chaining",
+                "@babel/plugin-proposal-nullish-coalescing-operator",
               ],
             },
           },
