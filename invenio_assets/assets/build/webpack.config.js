24bd89e368edc9f6502efeed259cec942e03fb33
mtorrisi <mario.torrisi@ingv.it>
2023-07-10 11:18:13 +0200

    Remove unneeded babel plugins

--- invenio_assets/assets/build/webpack.config.js
+++ invenio_assets/assets/build/webpack.config.js
@@ -128,8 +128,6 @@ var webpackConfig = {
               plugins: [
                 "@babel/plugin-proposal-class-properties",
                 "@babel/plugin-transform-runtime",
-                "@babel/plugin-proposal-optional-chaining",
-                "@babel/plugin-proposal-nullish-coalescing-operator",
               ],
             },
           },
